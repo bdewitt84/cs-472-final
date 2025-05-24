ab130ac58c792ef36af900921e0bd8611a3617f9
makanicb <makanicb25@gmail.com>
2025-05-24 15:22:35 -0700

    Test knn with simple logic functions and, or, xor

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -1,10 +1,33 @@
-import src.utils
+from src.utils import always_one, euclidian
 from src.knn import KNearestNeighbors
 
 def test_and():
-    model = KNearestNeighbors(5, utils.always_one, utils.euclidian)
+    model = KNearestNeighbors(1, always_one, euclidian)
     X = [[0,0],[0,1],[1,0],[1,1]]
     Y = [0, 0, 0, 1]
     model.fit(X, Y)
-    print(model.predict([0,0]))
+    assert model.predict([0,0]) == 0;
+    assert model.predict([0,1]) == 0;
+    assert model.predict([1,0]) == 0;
+    assert model.predict([1,1]) == 1;
+
+def test_or():
+    model = KNearestNeighbors(1, always_one, euclidian)
+    X = [[0,0],[0,1],[1,0],[1,1]]
+    Y = [0, 1, 1, 1]
+    model.fit(X, Y)
+    assert model.predict([0,0]) == 0;
+    assert model.predict([0,1]) == 1;
+    assert model.predict([1,0]) == 1;
+    assert model.predict([1,1]) == 1;
+
+def test_xor():
+    model = KNearestNeighbors(1, always_one, euclidian)
+    X = [[0,0],[0,1],[1,0],[1,1]]
+    Y = [0, 1, 1, 0]
+    model.fit(X, Y)
+    assert model.predict([0,0]) == 0;
+    assert model.predict([0,1]) == 1;
+    assert model.predict([1,0]) == 1;
+    assert model.predict([1,1]) == 0;
 
