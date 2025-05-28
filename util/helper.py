1cc1372e663714d50c5b25fc48614dd93018ade3
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-05-27 19:38:42 -0700

    Added accuracy_score to helper.py

--- util/helper.py
+++ util/helper.py
@@ -22,3 +22,11 @@ def safe_write(path, data):
         raise FileExistsError("path must not point to existing file")
     with open(path_obj, 'w') as file:
         file.write(data)
+
+
+def accuracy_score(y_pred, y_true):
+    if len(y_pred) != len(y_true):
+        raise ValueError("The y_pred and y_true have different lengths")
+
+    correct = [a == b for a, b in zip(y_pred, y_true)]
+    return sum(correct) / len(y_pred)
