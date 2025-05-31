c390cb1df1186c9ef881a07ab22f859674a893f4
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-05-30 20:09:00 -0700

    Added Pearson coefficient calculator

---
+++ src/correlation.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def correlation(features:pd.DataFrame, labels:pd.DataFrame):
+    """Calculates Pearson correlation between features and labels."""
+    feature_names = features.columns.tolist()
+    corrs:[(str, int)] = []
+    for feature in feature_names:
+        corr = features[feature].corr(labels)
+        corrs.append((feature, corr))
+    corrs.sort(key=lambda x: abs(x[1]), reverse=True)
+    return corrs
+
+if __name__ == '__main__':
+    from preprocessor.data_preprocessor import DataPreprocessor
+    from pprint import pprint
+
+    # Read in training data
+    train_data = pd.read_csv(r'data/train.csv')
+    train_df = pd.DataFrame(train_data)
+    train_dp = DataPreprocessor(train_df)
+
+    # Get training features and labels
+    train_features = train_dp.get_features('Survived')
+    train_labels = train_dp.get_labels('Survived')
+
+    corrs = correlation(train_features, train_labels)
+    pprint(corrs)
