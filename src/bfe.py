a86234e5911aca096359813f88853543d116167e
Brett DeWitt <38679865+bdewitt84@users.noreply.github.com>
2025-06-01 18:50:33 -0700

    Added proper docstring to bfe.py and ffi.py

--- src/bfe.py
+++ src/bfe.py
@@ -15,24 +15,37 @@ __date__ = "2025.5.30"
 
 
 def backward_feature_elimination(model, features:pd.DataFrame, labels:pd.DataFrame, evaluator:callable):
+    """
+    This function iteratively evaluates the model's performance by starting
+    with all features and then, in each round, removing the single feature
+    whose exclusion yields the highest evaluation score. The process continues
+    until no features remain. The best performing set is the set of features
+    that follow the highest recorded accuracy.
+    :param model: sklearn compatible model
+    :param features: dataframe of features to evaluate
+    :param labels: dataframe of labels to evaluate
+    :param evaluator: evaluator function that takes a model, features and labels
+    :return: list of tuples of features elimination order and the accuracy of
+             the features
+    """
     column_names = features.columns.tolist()
     drop_order: [(str, int)] = []
 
-    for _ in range(len(column_names)-1):
-        max_accuracy = 0
+    for _ in range(len(column_names)):
+        max_score = 0
         print(f"\n Considering: {column_names}")
         for column_name in column_names:
             print(f"Evaluating without {column_name}: ", end='')
             cur_features = features.drop(column_name, axis=1)
             score = evaluator(model, cur_features, labels)
             print(f"{score} ", end='')
-            if score > max_accuracy:
-                max_accuracy = score
+            if score > max_score:
+                max_score = score
                 drop_column = column_name
                 print("MAX", end='')
             print("\n", end='')
-        print("Excluding", drop_column, max_accuracy)
-        drop_order.append((drop_column, max_accuracy))
+        print("Excluding", drop_column, max_score)
+        drop_order.append((drop_column, max_score))
         features = features.drop(drop_column, axis=1)
         column_names.remove(drop_column)
 
@@ -41,7 +54,12 @@ def backward_feature_elimination(model, features:pd.DataFrame, labels:pd.DataFra
 
 
 if __name__ == "__main__":
-
+    """
+    Example script demonstrating the use of backward_feature_elimination()
+    Uses /data/train.csv to initialize a KNN classifier and writes
+    elimination order along with the accuracies associated with each
+    feature elimination to the output file.
+    """
     from preprocessor.data_preprocessor import DataPreprocessor
     from utils import always_one, euclidian
     from knn import KNearestNeighbors
@@ -58,7 +76,7 @@ if __name__ == "__main__":
     train_labels = train_dp.get_labels('Survived')
 
     params = {
-        "n_neighbors": 9,
+        "n_neighbors": 5,
         "weights": always_one,
         "metric": euclidian,
     }
@@ -67,3 +85,10 @@ if __name__ == "__main__":
 
     drop_order = backward_feature_elimination(classifier, train_features, train_labels, llocv)
     pprint(drop_order)
+
+    data = "feature,accuracy\n"
+    for feature, accuracy in drop_order:
+        data += f"{feature},{accuracy}\n"
+
+    with open("./eval_bfe_xval_out.csv", "w") as f:
+        f.write(data)
--- src/ffi.py
+++ src/ffi.py
@@ -15,27 +15,39 @@ __date__ = "2025.5.30"
 
 
 def forward_feature_inclusion(model, features:pd.DataFrame, label:pd.DataFrame, evaluator:callable):
+    """
+    This function iteratively evaluates the model's performance by starting
+    with an empty set of features and then, in each round, adding the single
+    feature whose inclusion yields the highest evaluation score. The process
+    continues until all available features have been added. The best performing
+    set of features is the set up to and including the highest recorded sore.
+    :param model:
+    :param features:
+    :param label:
+    :param evaluator:
+    :return:
+    """
     column_names = features.columns.tolist()
     columns = [features[column_name] for column_name in column_names]
     df = pd.DataFrame()
     inclusion_order:(str, int) = []
     for _ in range(len(columns)):
-        max_accuracy = 0
+        max_score = 0
         print(f"\nConsidering : {column_names}")
         for column_name in column_names:
             print(f"Evaluating with {column_name}: ", end='')
             current_df = pd.concat([df, features[column_name]], axis=1)
             accuracy = evaluator(model, current_df, label)
             print(f"{accuracy} ", end='')
-            if accuracy > max_accuracy:
-                max_accuracy = accuracy
+            if accuracy > max_score:
+                max_score = accuracy
                 max_column = column_name
                 print("MAX", end='')
             print("\n", end='')
-        print("Including: ", max_column, max_accuracy)
+        print("Including: ", max_column, max_score)
         column_names.remove(max_column)
         df = pd.concat([df, features[max_column]], axis=1)
-        inclusion_order.append((max_column, max_accuracy))
+        inclusion_order.append((max_column, max_score))
 
     return inclusion_order
 
@@ -68,3 +80,10 @@ if __name__ == "__main__":
 
     inclusion_order = forward_feature_inclusion(classifier, train_features, train_labels, llocv)
     pprint(inclusion_order)
+
+    data = "feature,accuracy\n"
+    for feature, accuracy in inclusion_order:
+        data += f"{feature},{accuracy}\n"
+
+    with open("./eval_ffi_xval_out.csv", "w") as f:
+        f.write(data)
