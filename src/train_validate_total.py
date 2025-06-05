ec6c1fcf94677320830bd3295cdcda7b620bf06a
makanicb <makanicb25@gmail.com>
2025-06-04 23:04:15 -0700

    Add models to github. Fixed total validate script

--- src/train_validate_total.py
+++ src/train_validate_total.py
@@ -40,12 +40,31 @@ if __name__ == "__main__":
     # Get concatenated data
     cat_df = pd.concat([train_df, val_df])
     cat_dp = DataPreprocessor(cat_df)
+
+    # Get bfe reduced data
+    bfe_df = cat_df.copy()
+    bfe_dp = DataPreprocessor(bfe_df)
+    bfe_dp.drop_features(["SibSp", "Fare", "Pclass_3", "Embarked_S"])
+   
+    # Get ffi reduced data
+    ffi_df = cat_df.copy()
+    ffi_dp = DataPreprocessor(ffi_df)
+    ffi_dp.drop_features(["Pclass_1", "Fare", "Pclass_2", "Embarked_Q"])
+
    
     # Get concatenated features and labels
     cat_features = cat_dp.get_features('Survived')
     cat_labels = cat_dp.get_labels('Survived')
     
-    with open("model_full.pkl", "rb") as f:
+    # Get bfe features and labels
+    bfe_features = bfe_dp.get_features('Survived')
+    bfe_labels = bfe_dp.get_labels('Survived')
+    
+    # Get ffi features and labels
+    ffi_features = ffi_dp.get_features('Survived')
+    ffi_labels = ffi_dp.get_labels('Survived')
+    
+    with open("models/model_full.pkl", "rb") as f:
         print("Loading full feature classifier")
         classifier = pickle.load(f)
 
@@ -55,37 +74,37 @@ if __name__ == "__main__":
         print("Accuracy: ", llocv(classifier, cat_features, cat_labels))
 
         print("Saving classifier")
-        with open("model_full_trained.pkl", "wb") as g:
+        with open("models/model_full_trained.pkl", "wb") as g:
             pickle.dump(classifier, g)
 
         print("Done") 
 
-    with open("model_bfe.pkl", "rb") as f:
+    with open("models/model_bfe.pkl", "rb") as f:
         print("Loading bfe feature classifier")
         classifier = pickle.load(f)
 
         print("Training classifier")
-        classifier.fit(cat_features, cat_labels)
+        classifier.fit(bfe_features, bfe_labels)
 
-        print("Accuracy: ", llocv(classifier, cat_features, cat_labels))
+        print("Accuracy: ", llocv(classifier, bfe_features, bfe_labels))
 
         print("Saving classifier")
-        with open("model_bfe_trained.pkl", "wb") as g:
+        with open("models/model_bfe_trained.pkl", "wb") as g:
             pickle.dump(classifier, g)
 
         print("Done") 
 
-    with open("model_ffi.pkl", "rb") as f:
+    with open("models/model_ffi.pkl", "rb") as f:
         print("Loading ffi feature classifier")
         classifier = pickle.load(f)
 
         print("Training classifier")
-        classifier.fit(cat_features, cat_labels)
+        classifier.fit(ffi_features, ffi_labels)
 
-        print("Accuracy: ", llocv(classifier, cat_features, cat_labels))
+        print("Accuracy: ", llocv(classifier, ffi_features, ffi_labels))
 
         print("Saving classifier")
-        with open("model_ffi_trained.pkl", "wb") as g:
+        with open("models/model_ffi_trained.pkl", "wb") as g:
             pickle.dump(classifier, g)
 
         print("Done") 
